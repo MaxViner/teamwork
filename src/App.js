2fd50c378d7834601ee9567208cf67f0e1a1fa17
max <kodtya.oreshin@inbox.ru>
2023-05-28 14:09:23 +0300

    GithubPush

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import { BrowserRouter } from 'react-router-dom'; 
 import { AppRouter } from './routes/AppRouter';
-import { Container } from './components/common/container/container';
+import { Container } from './components/common/container/AppContainer';
 function App() {
   return (
     <div className="App">
       <Container>
-      <BrowserRouter>
-        <Header/>
-        <AppRouter/>
+       <BrowserRouter>
+         <Header/>
+         <AppRouter/>
       </BrowserRouter> 
-
       </Container>
     </div>
   );
